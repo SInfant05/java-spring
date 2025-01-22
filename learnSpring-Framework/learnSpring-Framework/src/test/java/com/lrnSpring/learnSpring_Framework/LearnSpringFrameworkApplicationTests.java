077e1a09ff79324f9c1ccc565edf68a97256f7ae
SInfant05 <steve.apj.05@gmail.com>
2025-01-23 00:26:16 +0530

    Initialize Spring Boot project with basic configuration and structure -first commit

---
+++ learnSpring-Framework/learnSpring-Framework/src/test/java/com/lrnSpring/learnSpring_Framework/LearnSpringFrameworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lrnSpring.learnSpring_Framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringFrameworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
